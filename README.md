a762c1fcc1b44ec59a259d9c98f0d873047302a9
Hoshea26 <Hosheakaminski@gmail.com>
2023-05-06 02:07:12 +0300

    ok so this commit is actually a test

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a Git tutorial
+
+jbefiugqripurhghpakjbv;qelv
+    
+llqel;ejm;lhnl;kqen;kln;qekvbs
+m;alknv;qkljenv;lksnv
+sddn;slkn'LKVN'DLSKvni  
---
+++ main.py
@@ -0,0 +1,10 @@
+
+def print_hi(name, n):
+    print(f'Hi, {name}')
+    for i in range(n):
+        print(i)
+
+
+if __name__ == '__main__':
+    print_hi("knock knock", 10)
+
