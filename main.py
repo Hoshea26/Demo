44c79e07a20f1bf8f40a9fabfbf3513353d093f9
Hoshea26 <Hosheakaminski@gmail.com>
2023-05-06 02:45:43 +0300

    finished to develop the new function. a small step for me a giant leap for mankind

--- main.py
+++ main.py
@@ -4,7 +4,14 @@ def print_hi(name, n):
     for i in range(n):
         print(i)
 
+def print_f():
+    print(f'Hi, what is you name?')
+    var = input()
+    print(f"FUCK YOU {var}")
+
+
 
 if __name__ == '__main__':
     print_hi("knock knock", 10)
+    print_f()
 
